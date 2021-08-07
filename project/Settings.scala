282acf1f421edb6b463ebd8df87846a9cd596b53
Scala Steward <me@scala-steward.org>
2021-08-07 06:15:40 +0200

    Update fs2-core to 3.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -26,7 +26,7 @@ object Settings {
     val catsEffectVersion   = "3.2.0"
     val catsVersion         = "2.6.1"
     val mouseVersion        = "1.0.4"
-    val fs2Version          = "3.0.6"
+    val fs2Version          = "3.1.0"
     val shapelessVersion    = "2.3.7"
     val scalaParsersVersion = "1.1.2"
     val scalaXml            = "1.2.0"
