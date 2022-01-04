gemini-hlsw/seqexec

Set observer on run from
Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

modules/web/client/src/main/scala/seqexec/web/client/circuit/StepsTableAndStatusFocus.scala
@@ -14,6 +14,7 @@ import web.client.table._
 
 final case class StepsTableAndStatusFocus(
   status:           ClientStatus,
+  displayName:      Option[String],
   stepsTable:       Option[StepsTableFocus],
   tableState:       TableState[StepsTable.TableColumn],
   configTableState: TableState[StepConfigTable.TableColumn]
@@ -21,11 +22,14 @@ final case class StepsTableAndStatusFocus(
 
 object StepsTableAndStatusFocus {
   implicit val eq: Eq[StepsTableAndStatusFocus] =
-    Eq.by(x => (x.status, x.stepsTable, x.tableState, x.configTableState))
+    Eq.by(x => (x.status, x.displayName, x.stepsTable, x.tableState, x.configTableState))
 
   def stepsTableAndStatusFocusG(
     id: Observation.Id
-  ): Getter[SeqexecAppRootModel, StepsTableAndStatusFocus] =
+  ): Getter[SeqexecAppRootModel, StepsTableAndStatusFocus] = {
+
+    val displayNames =
+      SeqexecAppRootModel.userLoginFocus.asGetter
     ClientStatus.clientStatusFocusL.asGetter
       .zip(
         StepsTableFocus
@@ -34,10 +38,16 @@ object StepsTableAndStatusFocus {
             SeqexecAppRootModel
               .stepsTableStateL(id)
               .asGetter
-              .zip(SeqexecAppRootModel.configTableStateL.asGetter)
+              .zip(SeqexecAppRootModel.configTableStateL.asGetter.zip(displayNames))
           )
-      ) >>> { case (s, (f, (a, t))) =>
-      StepsTableAndStatusFocus(s, f, a.getOrElse(StepsTable.State.InitialTableState), t)
+      ) >>> { case (s, (f, (a, (t, dn)))) =>
+      StepsTableAndStatusFocus(s,
+                               dn.displayName,
+                               f,
+                               a.getOrElse(StepsTable.State.InitialTableState),
+                               t
+      )
     }
+  }
 
 }

modules/web/client/src/main/scala/seqexec/web/client/handlers/RemoteRequestsHandler.scala
@@ -42,11 +42,11 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
   }
 
   def handleRunFrom: PartialFunction[Any, ActionResult[M]] = {
-    case RequestRunFrom(id, stepId, options) =>
+    case RequestRunFrom(id, observer, stepId, options) =>
       val effect = value
         .map(clientId =>
           requestEffect(id,
-                        SeqexecWebClient.runFrom(_, stepId, clientId, options),
+                        SeqexecWebClient.runFrom(_, stepId, observer, clientId, options),
                         RunFromComplete(_, stepId),
                         RunFromFailed(_, stepId)
           )

modules/web/client/src/main/scala/seqexec/web/client/handlers/UserPromptHandler.scala
@@ -15,28 +15,30 @@ import seqexec.model.UserPrompt
 import seqexec.model.events.UserPromptNotification
 import seqexec.web.client.actions._
 import seqexec.web.client.model._
+import seqexec.web.client.circuit.UserPromptFocus
+import seqexec.model.Observer
 
-class UserPromptHandler[M](modelRW: ModelRW[M, UserPromptState])
+class UserPromptHandler[M](modelRW: ModelRW[M, UserPromptFocus])
     extends ActionHandler(modelRW)
-    with Handlers[M, UserPromptState] {
+    with Handlers[M, UserPromptFocus] {
+  val lens                                                          = UserPromptFocus.user ^|-> UserPromptState.notification
   def handleUserNotification: PartialFunction[Any, ActionResult[M]] = {
     case ServerMessage(UserPromptNotification(not, _)) =>
-      // Update the notification state
-      val lens         = UserPromptState.notification.set(not.some)
       // Update the model as load failed
       val modelUpdateE = not match {
         case UserPrompt.ChecksOverride(id, _, _) => Effect(Future(RunStartFailed(id)))
       }
-      updatedLE(lens, modelUpdateE)
+      updatedLE(lens.set(not.some), modelUpdateE)
   }
 
   def handleClosePrompt: PartialFunction[Any, ActionResult[M]] = { case CloseUserPromptBox(x) =>
-    val overrideEffect = this.value.notification match {
-      case Some(UserPrompt.ChecksOverride(id, stp, _)) if x === UserPromptResult.Cancel =>
-        Effect(Future(RequestRunFrom(id, stp, RunOptions.ChecksOverride)))
-      case _                                                                            => VoidEffect
+    val overrideEffect = (this.value.user.notification, this.value.displayName) match {
+      case (Some(UserPrompt.ChecksOverride(id, stp, _)), Some(dn))
+          if x === UserPromptResult.Cancel =>
+        Effect(Future(RequestRunFrom(id, Observer(dn), stp, RunOptions.ChecksOverride)))
+      case _ => VoidEffect
     }
-    updatedLE(UserPromptState.notification.set(none), overrideEffect)
+    updatedLE(lens.set(none), overrideEffect)
   }
 
   def handle: PartialFunction[Any, ActionResult[M]] =

gemini-hlsw/observe

modules/web/client/src/main/scala/observe/web/client/circuit/StepsTableAndStatusFocus.scala
@@ -14,6 +14,7 @@ import web.client.table._
 
 final case class StepsTableAndStatusFocus(
   status:           ClientStatus,
+  displayName:      Option[String],
   stepsTable:       Option[StepsTableFocus],
   tableState:       TableState[StepsTable.TableColumn],
   configTableState: TableState[StepConfigTable.TableColumn]
@@ -21,11 +22,14 @@ final case class StepsTableAndStatusFocus(
 
 object StepsTableAndStatusFocus {
   implicit val eq: Eq[StepsTableAndStatusFocus] =
-    Eq.by(x => (x.status, x.stepsTable, x.tableState, x.configTableState))
+    Eq.by(x => (x.status, x.displayName, x.stepsTable, x.tableState, x.configTableState))
 
   def stepsTableAndStatusFocusG(
     id: Observation.Id
-  ): Getter[ObserveAppRootModel, StepsTableAndStatusFocus] =
+  ): Getter[ObserveAppRootModel, StepsTableAndStatusFocus] = {
+
+    val displayNames =
+      ObserveAppRootModel.userLoginFocus.asGetter
     ClientStatus.clientStatusFocusL.asGetter
       .zip(
         StepsTableFocus
@@ -34,10 +38,16 @@ object StepsTableAndStatusFocus {
             ObserveAppRootModel
               .stepsTableStateL(id)
               .asGetter
-              .zip(ObserveAppRootModel.configTableStateL.asGetter)
+              .zip(ObserveAppRootModel.configTableStateL.asGetter.zip(displayNames))
           )
-      ) >>> { case (s, (f, (a, t))) =>
-      StepsTableAndStatusFocus(s, f, a.getOrElse(StepsTable.State.InitialTableState), t)
+      ) >>> { case (s, (f, (a, (t, dn)))) =>
+      StepsTableAndStatusFocus(s,
+                               dn.displayName,
+                               f,
+                               a.getOrElse(StepsTable.State.InitialTableState),
+                               t
+      )
     }
+  }
 
 }

modules/web/client/src/main/scala/observe/web/client/handlers/RemoteRequestsHandler.scala
@@ -42,11 +42,11 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
   }
 
   def handleRunFrom: PartialFunction[Any, ActionResult[M]] = {
-    case RequestRunFrom(idName, stepId, stepIdx, options) =>
+    case RequestRunFrom(idName, observer, stepId, stepIdx, options) =>
       val effect = value
         .map(clientId =>
           requestEffect(idName,
-                        ObserveWebClient.runFrom(_, stepId, clientId, options),
+                        ObserveWebClient.runFrom(_, stepId, observer, clientId, options),
                         RunFromComplete(_, stepId),
                         RunFromFailed(_, stepIdx)
           )

modules/web/client/src/main/scala/observe/web/client/handlers/UserPromptHandler.scala
@@ -16,27 +16,27 @@ import observe.model.events.UserPromptNotification
 import observe.web.client.actions._
 import observe.web.client.model._
 
-class UserPromptHandler[M](modelRW: ModelRW[M, UserPromptState])
+class UserPromptHandler[M](modelRW: ModelRW[M, UserPromptFocus])
     extends ActionHandler(modelRW)
-    with Handlers[M, UserPromptState] {
+    with Handlers[M, UserPromptFocus] {
+  val lens                                                          = UserPromptFocus.user ^|-> UserPromptState.notification
   def handleUserNotification: PartialFunction[Any, ActionResult[M]] = {
     case ServerMessage(UserPromptNotification(not, _)) =>
-      // Update the notification state
-      val lens         = UserPromptState.notification.replace(not.some)
       // Update the model as load failed
       val modelUpdateE = not match {
         case UserPrompt.ChecksOverride(idName, _, _, _) => Effect(Future(RunStartFailed(idName.id)))
       }
-      updatedLE(lens, modelUpdateE)
+      updatedLE(lens.set(not.some), modelUpdateE)
   }
 
   def handleClosePrompt: PartialFunction[Any, ActionResult[M]] = { case CloseUserPromptBox(x) =>
-    val overrideEffect = this.value.notification match {
-      case Some(UserPrompt.ChecksOverride(id, stp, sidx, _)) if x === UserPromptResult.Cancel =>
-        Effect(Future(RequestRunFrom(id, stp, sidx, RunOptions.ChecksOverride)))
-      case _                                                                                  => VoidEffect
+    val overrideEffect = (this.value.user.notification, this.value.displayName) match {
+      case (Some(UserPrompt.ChecksOverride(id, stp, sidx, _)), Some(dn))
+          if x === UserPromptResult.Cancel =>
+        Effect(Future(RequestRunFrom(id, Observer(dn), stp, sidx, RunOptions.ChecksOverride)))
+      case _ => VoidEffect
     }
-    updatedLE(UserPromptState.notification.replace(none), overrideEffect)
+    updatedLE(lens.replace(none), overrideEffect)
   }
 
   def handle: PartialFunction[Any, ActionResult[M]] =