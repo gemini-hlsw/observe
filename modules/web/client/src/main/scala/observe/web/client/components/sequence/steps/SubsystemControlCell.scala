gemini-hlsw/seqexec

Simplification refactoring
Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

modules/web/client/src/main/scala/seqexec/web/client/components/sequence/steps/SubsystemControlCell.scala
@@ -30,7 +30,6 @@ import seqexec.web.client.components.SeqexecStyles
 import seqexec.web.client.icons._
 import seqexec.web.client.model.ResourceRunOperation
 import seqexec.web.client.reusability._
-import seqexec.model.Observer
 
 /**
  * Contains the control buttons for each subsystem
@@ -40,8 +39,7 @@ final case class SubsystemControlCell(
   stepId:         Int,
   resources:      List[Resource],
   resourcesCalls: SortedMap[Resource, ResourceRunOperation],
-  canOperate:     Boolean,
-  displayName:    String
+  canOperate:     Boolean
 ) extends ReactProps[SubsystemControlCell](SubsystemControlCell.component)
 
 object SubsystemControlCell {
@@ -50,14 +48,13 @@ object SubsystemControlCell {
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
   def requestResourceCall(
-    id:       Observation.Id,
-    stepId:   StepId,
-    observer: Observer,
-    r:        Resource
+    id:     Observation.Id,
+    stepId: StepId,
+    r:      Resource
   ): (ReactMouseEvent, Button.ButtonProps) => Callback =
     (e: ReactMouseEvent, _: Button.ButtonProps) =>
       (e.preventDefaultCB *> e.stopPropagationCB *>
-        SeqexecCircuit.dispatchCB(RequestResourceRun(id, observer, stepId, r)))
+        SeqexecCircuit.dispatchCB(RequestResourceRun(id, stepId, r)))
         .unless_(e.altKey || e.button === StepsTable.MiddleButton)
 
   private val CompletedIcon = IconCheckmark.copy(
@@ -120,7 +117,7 @@ object SubsystemControlCell {
               labelPosition = labeled,
               icon = buttonIcon.isDefined,
               onClickE =
-                if (p.canOperate)(requestResourceCall(p.id, p.stepId, Observer(p.displayName), r))
+                if (p.canOperate)(requestResourceCall(p.id, p.stepId, r))
                 else js.undefined,
               clazz = SeqexecStyles.defaultCursor.unless_(p.canOperate)
             )(buttonIcon.whenDefined(identity), r.show)

modules/web/client/src/main/scala/seqexec/web/client/components/sequence/toolbars/SequenceControl.scala
@@ -38,7 +38,6 @@ import seqexec.web.client.model.SyncOperation
 import seqexec.web.client.reusability._
 import seqexec.web.client.semanticui.controlButton
 import seqexec.web.client.services.SeqexecWebClient
-import seqexec.model.Observer
 
 final case class SequenceControl(p: SequenceControlFocus)
     extends ReactProps[SequenceControl](SequenceControl.component) {
@@ -82,8 +81,8 @@ object SequenceControl {
 
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
-  def requestRun(s: Observation.Id, observer: Observer): Callback =
-    SeqexecCircuit.dispatchCB(RequestRun(s, observer, RunOptions.Normal))
+  def requestRun(s: Observation.Id): Callback =
+    SeqexecCircuit.dispatchCB(RequestRun(s, RunOptions.Normal))
 
   def requestSync(s: Observation.Id): Callback =
     SeqexecCircuit.dispatchCB(RequestSync(s))
@@ -107,8 +106,7 @@ object SequenceControl {
     id:                  Observation.Id,
     isPartiallyExecuted: Boolean,
     nextStepToRun:       Int,
-    canRun:              Boolean,
-    displayName:         String
+    canRun:              Boolean
   ) = {
     val runContinueTooltip =
       s"${isPartiallyExecuted.fold("Continue", "Run")} the sequence from the step $nextStepToRun"
@@ -116,7 +114,7 @@ object SequenceControl {
       s"${isPartiallyExecuted.fold("Continue", "Run")} from step $nextStepToRun"
     controlButton(icon = IconPlay,
                   color = Blue,
-                  onClick = requestRun(id, Observer(displayName)),
+                  onClick = requestRun(id),
                   disabled = !canRun,
                   tooltip = runContinueTooltip,
                   text = runContinueButton
@@ -184,9 +182,9 @@ object SequenceControl {
     ScalaComponent
       .builder[Props]
       .renderP { ($, p) =>
-        val SequenceControlFocus(_, _, overrides, _, _, control, displayName) = p.p
-        val ControlModel(id, partial, nextStep, status, _)                    = control
-        val nextStepToRun                                                     = nextStep.foldMap(_ + 1)
+        val SequenceControlFocus(_, _, overrides, _, _, control) = p.p
+        val ControlModel(id, partial, nextStep, status, _)       = control
+        val nextStepToRun                                        = nextStep.foldMap(_ + 1)
 
         <.div(
           SeqexecStyles.SequenceControlForm,
@@ -195,7 +193,7 @@ object SequenceControl {
             syncButton(id, p.canSync)
               .when(status.isIdle || status.isError),
             // Run button
-            runButton(id, partial, nextStepToRun, p.canRun, displayName)
+            runButton(id, partial, nextStepToRun, p.canRun)
               .when(status.isIdle || status.isError),
             // Cancel pause button
             cancelPauseButton(id, p.canCancelPause)

modules/web/client/src/main/scala/seqexec/web/client/model/ClientStatus.scala
@@ -7,24 +7,39 @@ import cats.Eq
 import monocle.Getter
 import monocle.Lens
 import seqexec.model.UserDetails
+import seqexec.model.ClientId
+import seqexec.model.Observer
 
 /**
  * Utility class to let components more easily switch parts of the UI depending on the user and
  * connection state
  */
-final case class ClientStatus(user: Option[UserDetails], w: WebSocketConnection) {
-  def isLogged: Boolean    = user.isDefined
-  def isConnected: Boolean = w.ws.isReady
-  def canOperate: Boolean  = isLogged && isConnected
+final case class ClientStatus(
+  user:         Option[UserDetails],
+  clientId:     Option[ClientId],
+  displayNames: Map[String, String],
+  w:            WebSocketConnection
+) {
+  def isLogged: Boolean           = user.isDefined
+  def isConnected: Boolean        = w.ws.isReady
+  def canOperate: Boolean         = isLogged && isConnected
+  def displayName: Option[String] = user.flatMap(u => displayNames.get(u.username))
+  def observer: Option[Observer]  = displayName.map(Observer.apply)
 }
 
 object ClientStatus {
   implicit val eq: Eq[ClientStatus] =
-    Eq.by(x => (x.user, x.w))
+    Eq.by(x => (x.user, x.clientId, x.displayNames, x.w))
 
   val clientStatusFocusL: Lens[SeqexecAppRootModel, ClientStatus] =
-    Lens[SeqexecAppRootModel, ClientStatus](m => ClientStatus(m.uiModel.user, m.ws))(v =>
-      m => m.copy(ws = v.w, uiModel = m.uiModel.copy(user = v.user))
+    Lens[SeqexecAppRootModel, ClientStatus](m =>
+      ClientStatus(m.uiModel.user, m.clientId, m.uiModel.displayNames, m.ws)
+    )(v =>
+      m =>
+        m.copy(ws = v.w,
+               clientId = v.clientId,
+               uiModel = m.uiModel.copy(user = v.user, displayNames = v.displayNames)
+        )
     )
 
   val canOperateG: Getter[SeqexecAppRootModel, Boolean] =

gemini-hlsw/observe

modules/web/client/src/main/scala/observe/web/client/components/sequence/steps/SubsystemControlCell.scala
@@ -38,8 +38,7 @@ final case class SubsystemControlCell(
   stepId:         StepId,
   resources:      List[Resource],
   resourcesCalls: SortedMap[Resource, ResourceRunOperation],
-  canOperate:     Boolean,
-  displayName:    String
+  canOperate:     Boolean
 ) extends ReactProps[SubsystemControlCell](SubsystemControlCell.component)
 
 object SubsystemControlCell {
@@ -48,14 +47,13 @@ object SubsystemControlCell {
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
   def requestResourceCall(
-    idName: Observation.IdName,
+    id:     Observation.Id,
     stepId: StepId,
-    observer: Observer,
     r:      Resource
   ): (ReactMouseEvent, Button.ButtonProps) => Callback =
     (e: ReactMouseEvent, _: Button.ButtonProps) =>
       (e.preventDefaultCB *> e.stopPropagationCB *>
-        ObserveCircuit.dispatchCB(RequestResourceRun(idName, observer, stepId, r)))
+        ObserveCircuit.dispatchCB(RequestResourceRun(id, stepId, r)))
         .unless_(e.altKey || e.button === StepsTable.MiddleButton)
 
   private val CompletedIcon = IconCheckmark.copy(
@@ -96,7 +94,7 @@ object SubsystemControlCell {
       case _                                                  => none
     }
 
-  protected val component: Component[Props, Unit, Unit, CtorType.Props] = ScalaComponent
+  protected val component = ScalaComponent
     .builder[Props]
     .render_P { p =>
       <.div(
@@ -118,7 +116,8 @@ object SubsystemControlCell {
               labelPosition = labeled,
               icon = buttonIcon.isDefined,
               onClickE =
-                if (p.canOperate) requestResourceCall(p.idName, p.stepId, Observer(p.displayName), r) else js.undefined,
+                if (p.canOperate)(requestResourceCall(p.id, p.stepId, r))
+                else js.undefined,
               clazz = ObserveStyles.defaultCursor.unless_(p.canOperate)
             )(buttonIcon.whenDefined(identity), r.show)
           )(s"Configure ${r.show}")

modules/web/client/src/main/scala/observe/web/client/components/sequence/toolbars/SequenceControl.scala
@@ -79,8 +79,8 @@ object SequenceControl {
 
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
-  def requestRun(id: Observation.Id, observer: Observer): Callback =
-    ObserveCircuit.dispatchCB(RequestRun(id, observer, RunOptions.Normal))
+  def requestRun(s: Observation.Id): Callback =
+    ObserveCircuit.dispatchCB(RequestRun(s, RunOptions.Normal))
 
   def requestSync(idName: Observation.IdName): Callback =
     ObserveCircuit.dispatchCB(RequestSync(idName))
@@ -104,8 +104,7 @@ object SequenceControl {
     id:                  Observation.Id,
     isPartiallyExecuted: Boolean,
     nextStepToRun:       Int,
-    canRun:              Boolean,
-    displayName:         String
+    canRun:              Boolean
   ) = {
     val runContinueTooltip =
       s"${isPartiallyExecuted.fold("Continue", "Run")} the sequence from the step $nextStepToRun"
@@ -113,7 +112,7 @@ object SequenceControl {
       s"${isPartiallyExecuted.fold("Continue", "Run")} from step $nextStepToRun"
     controlButton(icon = IconPlay,
                   color = Blue,
-                  onClick = requestRun(id, Observer(displayName)),
+                  onClick = requestRun(id),
                   disabled = !canRun,
                   tooltip = runContinueTooltip,
                   text = runContinueButton
@@ -181,9 +180,9 @@ object SequenceControl {
     ScalaComponent
       .builder[Props]
       .renderP { ($, p) =>
-        val SequenceControlFocus(_, _, overrides, _, _, control, displayName)  = p.p
-        val ControlModel(idName, partial, nextStepIdx, status, _)              = control
-        val nextStepToRunIdx                                                   = nextStepIdx.foldMap(_ + 1)
+        val SequenceControlFocus(_, _, overrides, _, _, control) = p.p
+        val ControlModel(idName, partial, nextStepIdx, status, _)       = control
+        val nextStepToRunIdx                                        = nextStep.foldMap(_ + 1)
 
         <.div(
           ObserveStyles.SequenceControlForm,
@@ -192,7 +191,7 @@ object SequenceControl {
             syncButton(idName, p.canSync)
               .when(status.isIdle || status.isError),
             // Run button
-            runButton(id, partial, nextStepToRun, p.canRun, displayName)
+            runButton(id, partial, nextStepToRun, p.canRun)
               .when(status.isIdle || status.isError),
             // Cancel pause button
             cancelPauseButton(idName.id, p.canCancelPause)

modules/web/client/src/main/scala/observe/web/client/model/ClientStatus.scala
@@ -12,19 +12,32 @@ import observe.model.UserDetails
  * Utility class to let components more easily switch parts of the UI depending on the user and
  * connection state
  */
-final case class ClientStatus(user: Option[UserDetails], w: WebSocketConnection) {
-  def isLogged: Boolean    = user.isDefined
-  def isConnected: Boolean = w.ws.isReady
-  def canOperate: Boolean  = isLogged && isConnected
+final case class ClientStatus(
+  user:         Option[UserDetails],
+  clientId:     Option[ClientId],
+  displayNames: Map[String, String],
+  w:            WebSocketConnection
+) {
+  def isLogged: Boolean           = user.isDefined
+  def isConnected: Boolean        = w.ws.isReady
+  def canOperate: Boolean         = isLogged && isConnected
+  def displayName: Option[String] = user.flatMap(u => displayNames.get(u.username))
+  def observer: Option[Observer]  = displayName.map(Observer.apply)
 }
 
 object ClientStatus {
   implicit val eq: Eq[ClientStatus] =
-    Eq.by(x => (x.user, x.w))
+    Eq.by(x => (x.user, x.clientId, x.displayNames, x.w))
 
   val clientStatusFocusL: Lens[ObserveAppRootModel, ClientStatus] =
-    Lens[ObserveAppRootModel, ClientStatus](m => ClientStatus(m.uiModel.user, m.ws))(v =>
-      m => m.copy(ws = v.w, uiModel = m.uiModel.copy(user = v.user))
+    Lens[ObserveAppRootModel, ClientStatus](m =>
+      ClientStatus(m.uiModel.user, m.clientId, m.uiModel.displayNames, m.ws)
+    )(v =>
+      m =>
+        m.copy(ws = v.w,
+               clientId = v.clientId,
+               uiModel = m.uiModel.copy(user = v.user, displayNames = v.displayNames)
+        )
     )
 
   val canOperateG: Getter[ObserveAppRootModel, Boolean] =