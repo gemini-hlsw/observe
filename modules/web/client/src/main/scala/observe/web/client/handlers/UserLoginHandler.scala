gemini-hlsw/seqexec

Persist own display names
Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

modules/web/client/src/main/scala/seqexec/web/client/handlers/UserLoginHandler.scala
@@ -6,6 +6,7 @@ package seqexec.web.client.handlers
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
+import cats.Eq
 import cats.syntax.all._
 import diode.ActionHandler
 import diode.ActionResult
@@ -16,32 +17,44 @@ import seqexec.common.HttpStatusCodes
 import seqexec.model.UserDetails
 import seqexec.web.client.actions._
 import seqexec.web.client.services.SeqexecWebClient
+import seqexec.web.client.services.DisplayNamePersistence
+
+final case class UserLoginFocus(user: Option[UserDetails], displayNames: Map[String, String])
+
+object UserLoginFocus {
+  implicit val eqUserLoginFocus: Eq[UserLoginFocus] = Eq.by(u => (u.user, u.displayNames))
+}
 
 /**
  * Handles actions related to opening/closing the login box
  */
-class UserLoginHandler[M](modelRW: ModelRW[M, Option[UserDetails]])
+class UserLoginHandler[M](modelRW: ModelRW[M, UserLoginFocus])
     extends ActionHandler(modelRW)
-    with Handlers[M, Option[UserDetails]] {
+    with Handlers[M, UserLoginFocus]
+    with DisplayNamePersistence {
   override def handle: PartialFunction[Any, ActionResult[M]] = {
     case LoggedIn(u) =>
+      val dn               = value.displayNames + (u.username -> u.displayName)
+      val resetDisplayName = Effect(persistDisplayName(dn).as(NoAction))
       // Close the login box
-      val effect    = Effect(Future(CloseLoginBox))
+      val effect           = Effect(Future(CloseLoginBox))
       // Close the websocket and reconnect
-      val reconnect = Effect(Future(Reconnect))
-      updated(Some(u), reconnect + effect)
+      val reconnect        = Effect(Future(Reconnect))
+      updated(value.copy(user = u.some, displayNames = dn), reconnect + effect + resetDisplayName)
 
     case VerifyLoggedStatus =>
       val effect = Effect(SeqexecWebClient.ping().map {
-        case HttpStatusCodes.Unauthorized if value.isDefined => Logout
-        case _                                               => NoAction
+        case HttpStatusCodes.Unauthorized if value.user.isDefined => Logout
+        case _                                                    => NoAction
       })
       effectOnly(effect)
 
     case Logout =>
-      val effect    = Effect(SeqexecWebClient.logout().as(NoAction))
-      val reConnect = Effect(Future(Reconnect))
+      val dn               = value.displayNames.removed(value.user.foldMap(_.username))
+      val cleanDisplayName = Effect(removeDisplayName(dn).as(NoAction))
+      val effect           = Effect(SeqexecWebClient.logout().as(NoAction))
+      val reConnect        = Effect(Future(Reconnect))
       // Remove the user and call logout
-      updated(None, effect + reConnect)
+      updated(value.copy(user = none, displayNames = dn), effect + reConnect + cleanDisplayName)
   }
 }

modules/web/client/src/main/scala/seqexec/web/client/handlers/handlers.scala
@@ -12,11 +12,11 @@ import diode.Effect
 import diode.ModelRW
 import diode.NoAction
 import lucuma.core.enum.Site
-import seqexec.model.Observer
 import seqexec.model.Operator
 import seqexec.web.client.actions._
 import seqexec.web.client.model.GlobalLog
 import seqexec.web.client.services.SeqexecWebClient
+import seqexec.web.client.services.DisplayNamePersistence
 
 /**
  * Handles updates to the operator
@@ -27,6 +27,22 @@ class OperatorHandler[M](modelRW: ModelRW[M, Option[Operator]])
   override def handle: PartialFunction[Any, ActionResult[M]] = { case UpdateOperator(name) =>
     val updateOperatorE = Effect(SeqexecWebClient.setOperator(name).as(NoAction))
     updated(name.some, updateOperatorE)
+  }
+}
+
+/**
+ * Handles updates to the operator
+ */
+class DisplayNameHandler[M](modelRW: ModelRW[M, Map[String, String]])
+    extends ActionHandler(modelRW)
+    with Handlers[M, Map[String, String]]
+    with DisplayNamePersistence {
+
+  override def handle: PartialFunction[Any, ActionResult[M]] = {
+    case UpdateDisplayName(username, dn) =>
+      val result    = value + (username -> dn)
+      val persistDN = Effect(persistDisplayName(result).as(NoAction))
+      updated(result, persistDN)
   }
 }
 
@@ -54,16 +70,5 @@ class GlobalLogHandler[M](modelRW: ModelRW[M, GlobalLog])
 
     case ToggleLogArea =>
       updated(value.copy(display = value.display.toggle))
-  }
-}
-
-/**
- * Handles updates to the defaultObserver
- */
-class DefaultObserverHandler[M](modelRW: ModelRW[M, Observer])
-    extends ActionHandler(modelRW)
-    with Handlers[M, Observer] {
-  override def handle: PartialFunction[Any, ActionResult[M]] = { case UpdateDefaultObserver(o) =>
-    updated(o)
   }
 }

gemini-hlsw/observe

modules/web/client/src/main/scala/observe/web/client/handlers/UserLoginHandler.scala
@@ -6,6 +6,7 @@ package observe.web.client.handlers
 import scala.concurrent.Future
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 
+import cats.Eq
 import cats.syntax.all._
 import diode.ActionHandler
 import diode.ActionResult
@@ -16,32 +17,44 @@ import observe.common.HttpStatusCodes
 import observe.model.UserDetails
 import observe.web.client.actions._
 import observe.web.client.services.ObserveWebClient
+import observe.web.client.services.DisplayNamePersistence
+
+final case class UserLoginFocus(user: Option[UserDetails], displayNames: Map[String, String])
+
+object UserLoginFocus {
+  implicit val eqUserLoginFocus: Eq[UserLoginFocus] = Eq.by(u => (u.user, u.displayNames))
+}
 
 /**
  * Handles actions related to opening/closing the login box
  */
-class UserLoginHandler[M](modelRW: ModelRW[M, Option[UserDetails]])
+class UserLoginHandler[M](modelRW: ModelRW[M, UserLoginFocus])
     extends ActionHandler(modelRW)
-    with Handlers[M, Option[UserDetails]] {
+    with Handlers[M, UserLoginFocus]
+    with DisplayNamePersistence {
   override def handle: PartialFunction[Any, ActionResult[M]] = {
     case LoggedIn(u) =>
+      val dn               = value.displayNames + (u.username -> u.displayName)
+      val resetDisplayName = Effect(persistDisplayName(dn).as(NoAction))
       // Close the login box
-      val effect    = Effect(Future(CloseLoginBox))
+      val effect           = Effect(Future(CloseLoginBox))
       // Close the websocket and reconnect
-      val reconnect = Effect(Future(Reconnect))
-      updated(Some(u), reconnect + effect)
+      val reconnect        = Effect(Future(Reconnect))
+      updated(value.copy(user = u.some, displayNames = dn), reconnect + effect + resetDisplayName)
 
     case VerifyLoggedStatus =>
       val effect = Effect(ObserveWebClient.ping().map {
-        case HttpStatusCodes.Unauthorized if value.isDefined => Logout
-        case _                                               => NoAction
+        case HttpStatusCodes.Unauthorized if value.user.isDefined => Logout
+        case _                                                    => NoAction
       })
       effectOnly(effect)
 
     case Logout =>
-      val effect    = Effect(ObserveWebClient.logout().as(NoAction))
-      val reConnect = Effect(Future(Reconnect))
+      val dn               = value.displayNames.removed(value.user.foldMap(_.username))
+      val cleanDisplayName = Effect(removeDisplayName(dn).as(NoAction))
+      val effect           = Effect(ObserveWebClient.logout().as(NoAction))
+      val reConnect        = Effect(Future(Reconnect))
       // Remove the user and call logout
-      updated(None, effect + reConnect)
+      updated(value.copy(user = none, displayNames = dn), effect + reConnect + cleanDisplayName)
   }
 }

modules/web/client/src/main/scala/observe/web/client/handlers/handlers.scala
@@ -17,6 +17,7 @@ import observe.model.Operator
 import observe.web.client.actions._
 import observe.web.client.model.GlobalLog
 import observe.web.client.services.ObserveWebClient
+import observe.web.client.services.DisplayNamePersistence
 
 /**
  * Handles updates to the operator
@@ -27,6 +28,22 @@ class OperatorHandler[M](modelRW: ModelRW[M, Option[Operator]])
   override def handle: PartialFunction[Any, ActionResult[M]] = { case UpdateOperator(name) =>
     val updateOperatorE = Effect(ObserveWebClient.setOperator(name).as(NoAction))
     updated(name.some, updateOperatorE)
+  }
+}
+
+/**
+ * Handles updates to the operator
+ */
+class DisplayNameHandler[M](modelRW: ModelRW[M, Map[String, String]])
+    extends ActionHandler(modelRW)
+    with Handlers[M, Map[String, String]]
+    with DisplayNamePersistence {
+
+  override def handle: PartialFunction[Any, ActionResult[M]] = {
+    case UpdateDisplayName(username, dn) =>
+      val result    = value + (username -> dn)
+      val persistDN = Effect(persistDisplayName(result).as(NoAction))
+      updated(result, persistDN)
   }
 }
 
@@ -54,16 +71,5 @@ class GlobalLogHandler[M](modelRW: ModelRW[M, GlobalLog])
 
     case ToggleLogArea =>
       updated(value.copy(display = value.display.toggle))
-  }
-}
-
-/**
- * Handles updates to the defaultObserver
- */
-class DefaultObserverHandler[M](modelRW: ModelRW[M, Observer])
-    extends ActionHandler(modelRW)
-    with Handlers[M, Observer] {
-  override def handle: PartialFunction[Any, ActionResult[M]] = { case UpdateDefaultObserver(o) =>
-    updated(o)
   }
 }