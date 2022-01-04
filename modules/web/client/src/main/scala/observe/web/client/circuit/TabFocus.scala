gemini-hlsw/seqexec

Set observer on run
Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

modules/web/client/src/main/scala/seqexec/web/client/circuit/TabFocus.scala
@@ -7,27 +7,26 @@ import cats.Eq
 import cats.data.NonEmptyList
 import cats.syntax.all._
 import monocle.Getter
-// import seqexec.model._
 import seqexec.web.client.model._
 
 final case class TabFocus(
-  canOperate: Boolean,
-  tabs:       NonEmptyList[Either[CalibrationQueueTabActive, AvailableTab]]
-  // defaultObserver: Observer
+  canOperate:  Boolean,
+  tabs:        NonEmptyList[Either[CalibrationQueueTabActive, AvailableTab]],
+  displayName: Option[String]
 )
 
 object TabFocus {
   implicit val eq: Eq[TabFocus] =
-    Eq.by(x => (x.canOperate, x.tabs)) //, x.defaultObserver))
+    Eq.by(x => (x.canOperate, x.tabs, x.displayName))
 
   val tabFocusG: Getter[SeqexecAppRootModel, TabFocus] = {
     val getter = SeqexecAppRootModel.uiModel.composeGetter(
       (SeqexecUIModel.sequencesOnDisplay
         .composeGetter(SequencesOnDisplay.availableTabsG))
-      // .zip(SeqexecUIModel.defaultObserverG)
+        .zip(SeqexecUIModel.displayNameG)
     )
-    ClientStatus.canOperateG.zip(getter) >>> { case (o, t) =>
-      TabFocus(o, t) //, ob)
+    ClientStatus.canOperateG.zip(getter) >>> { case (o, (t, ob)) =>
+      TabFocus(o, t, ob)
     }
   }
 

modules/web/client/src/main/scala/seqexec/web/client/circuit/package.scala
@@ -213,7 +213,8 @@ package circuit {
     systemOverrides:        SystemOverrides,
     overrideSubsysControls: SectionVisibilityState,
     canOperate:             Boolean,
-    control:                ControlModel
+    control:                ControlModel,
+    displayName:            String
   )
 
   object SequenceControlFocus {
@@ -224,25 +225,31 @@ package circuit {
          x.systemOverrides,
          x.overrideSubsysControls,
          x.canOperate,
-         x.control
+         x.control,
+         x.displayName
         )
       )
 
     def seqControlG(
       id: Observation.Id
     ): Getter[SeqexecAppRootModel, Option[SequenceControlFocus]] = {
-      val getter =
+      val displayNames =
+        SeqexecAppRootModel.userLoginFocus.asGetter
+      val tabGetter    =
         SeqexecAppRootModel.sequencesOnDisplayL.composeGetter(SequencesOnDisplay.tabG(id))
-      ClientStatus.canOperateG.zip(getter) >>> {
-        case (status, Some(SeqexecTabActive(tab, _))) =>
+      ClientStatus.canOperateG.zip(tabGetter.zip(displayNames)) >>> {
+        case (status,
+              (Some(SeqexecTabActive(tab, _)), UserLoginFocus(Some(UserDetails(u, _)), dn))
+            ) =>
           SequenceControlFocus(tab.instrument,
                                tab.obsId,
                                tab.sequence.systemOverrides,
                                tab.subsystemControlVisible,
                                status,
-                               ControlModel.controlModelG.get(tab)
+                               ControlModel.controlModelG.get(tab),
+                               dn.get(u).orEmpty
           ).some
-        case _                                        => none
+        case _ => none
       }
     }
   }

modules/web/client/src/main/scala/seqexec/web/client/components/SessionQueueTable.scala
@@ -56,8 +56,10 @@ trait Columns {
   val InstrumentMinWidth    = 90.4333 + SeqexecStyles.TableBorderWidth
   val TargetNameColumnWidth = 140.0
   val TargetMinWidth        = 60.0167 + SeqexecStyles.TableBorderWidth
-  val ObsNameColumnWidth    = 140.0
+  val ObsNameColumnWidth    = 100.0
   val ObsNameMinWidth       = 89.7340 + SeqexecStyles.TableBorderWidth
+  val ObserverColumnWidth   = 100.0
+  val ObserverMinWidth      = 89.7340 + SeqexecStyles.TableBorderWidth
 
   sealed trait TableColumn     extends Product with Serializable
   case object IconColumn       extends TableColumn
@@ -68,6 +70,7 @@ trait Columns {
   case object InstrumentColumn extends TableColumn
   case object ObsNameColumn    extends TableColumn
   case object TargetNameColumn extends TableColumn
+  case object ObserverColumn   extends TableColumn
 
   object TableColumn {
     implicit val equal: Eq[TableColumn] = Eq.fromUniversalEquals
@@ -146,14 +149,26 @@ trait Columns {
     width = VariableColumnWidth.unsafeFromDouble(0.25, ObsNameMinWidth)
   )
 
-  val all: NonEmptyList[ColumnMeta[TableColumn]] = NonEmptyList.of(IconColumnMeta,
-                                                                   AddQueueColumnMeta,
-                                                                   ClassColumnMeta,
-                                                                   ObsIdColumnMeta,
-                                                                   StateColumnMeta,
-                                                                   InstrumentColumnMeta,
-                                                                   TargetNameColumnMeta,
-                                                                   ObsNameColumnMeta
+  val ObserverColumnMeta: ColumnMeta[TableColumn] = ColumnMeta[TableColumn](
+    ObserverColumn,
+    name = "observer",
+    label = "Observer",
+    visible = true,
+    removeable = 3,
+    grow = 3,
+    width = VariableColumnWidth.unsafeFromDouble(0.25, ObserverMinWidth)
+  )
+
+  val all: NonEmptyList[ColumnMeta[TableColumn]] = NonEmptyList.of(
+    IconColumnMeta,
+    AddQueueColumnMeta,
+    ClassColumnMeta,
+    ObsIdColumnMeta,
+    StateColumnMeta,
+    InstrumentColumnMeta,
+    TargetNameColumnMeta,
+    ObsNameColumnMeta,
+    ObserverColumnMeta
   )
 
   val allTC = all.map(_.column)
@@ -166,7 +181,8 @@ trait Columns {
     StateColumn      -> StateColumnWidth,
     InstrumentColumn -> InstrumentColumnWidth,
     TargetNameColumn -> TargetNameColumnWidth,
-    ObsNameColumn    -> ObsNameColumnWidth
+    ObsNameColumn    -> ObsNameColumnWidth,
+    ObserverColumn   -> ObserverColumnWidth
   )
 
   val columnsMinWidth: Map[TableColumn, Double] = Map(
@@ -174,7 +190,8 @@ trait Columns {
     StateColumn      -> StateMinWidth,
     InstrumentColumn -> InstrumentMinWidth,
     TargetNameColumn -> TargetMinWidth,
-    ObsNameColumn    -> ObsNameMinWidth
+    ObsNameColumn    -> ObsNameMinWidth,
+    ObserverColumn   -> ObserverMinWidth
   )
 }
 
@@ -195,6 +212,7 @@ object SessionQueueTable extends Columns {
                           s.status,
                           s.instrument,
                           s.targetName,
+                          s.observer,
                           s.name,
                           s.obsClass,
                           s.active,
@@ -289,6 +307,7 @@ object SessionQueueTable extends Columns {
     var status: SequenceState
     var instrument: Instrument
     var targetName: Option[String]
+    var observer: Option[Observer]
     var name: String
     var obsClass: ObsClass
     var active: Boolean
@@ -305,6 +324,7 @@ object SessionQueueTable extends Columns {
       status:        SequenceState,
       instrument:    Instrument,
       targetName:    Option[String],
+      observer:      Option[Observer],
       name:          String,
       obsClass:      ObsClass,
       active:        Boolean,
@@ -318,6 +338,7 @@ object SessionQueueTable extends Columns {
       p.status = status
       p.instrument = instrument
       p.targetName = targetName
+      p.observer = observer
       p.name = name
       p.obsClass = obsClass
       p.active = active
@@ -334,6 +355,7 @@ object SessionQueueTable extends Columns {
         SequenceState,
         Instrument,
         Option[String],
+        Option[Observer],
         String,
         ObsClass,
         Boolean,
@@ -348,6 +370,7 @@ object SessionQueueTable extends Columns {
          l.status,
          l.instrument,
          l.targetName,
+         l.observer,
          l.name,
          l.obsClass,
          l.active,
@@ -363,6 +386,7 @@ object SessionQueueTable extends Columns {
         Observation.Id.unsafeFromString("Zero-1"),
         SequenceState.Idle,
         Instrument.F2,
+        None,
         None,
         "",
         ObsClass.Nighttime,
@@ -554,6 +578,7 @@ object SessionQueueTable extends Columns {
     case InstrumentColumn => linkedTextRenderer(b.props)(_.instrument.show)
     case TargetNameColumn => linkedTextRenderer(b.props)(_.targetName.getOrElse(UnknownTargetName))
     case ObsNameColumn    => linkedTextRenderer(b.props)(_.name)
+    case ObserverColumn   => linkedTextRenderer(b.props)(_.observer.foldMap(_.value))
   }
 
   private val fixedHeaderRenderer: TableColumn => HeaderRenderer[js.Object] = {

modules/web/client/src/main/scala/seqexec/web/client/components/sequence/toolbars/SequenceControl.scala
@@ -38,6 +38,7 @@ import seqexec.web.client.model.SyncOperation
 import seqexec.web.client.reusability._
 import seqexec.web.client.semanticui.controlButton
 import seqexec.web.client.services.SeqexecWebClient
+import seqexec.model.Observer
 
 final case class SequenceControl(p: SequenceControlFocus)
     extends ReactProps[SequenceControl](SequenceControl.component) {
@@ -81,8 +82,8 @@ object SequenceControl {
 
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
-  def requestRun(s: Observation.Id): Callback =
-    SeqexecCircuit.dispatchCB(RequestRun(s, RunOptions.Normal))
+  def requestRun(s: Observation.Id, observer: Observer): Callback =
+    SeqexecCircuit.dispatchCB(RequestRun(s, observer, RunOptions.Normal))
 
   def requestSync(s: Observation.Id): Callback =
     SeqexecCircuit.dispatchCB(RequestSync(s))
@@ -106,7 +107,8 @@ object SequenceControl {
     id:                  Observation.Id,
     isPartiallyExecuted: Boolean,
     nextStepToRun:       Int,
-    canRun:              Boolean
+    canRun:              Boolean,
+    displayName:         String
   ) = {
     val runContinueTooltip =
       s"${isPartiallyExecuted.fold("Continue", "Run")} the sequence from the step $nextStepToRun"
@@ -114,7 +116,7 @@ object SequenceControl {
       s"${isPartiallyExecuted.fold("Continue", "Run")} from step $nextStepToRun"
     controlButton(icon = IconPlay,
                   color = Blue,
-                  onClick = requestRun(id),
+                  onClick = requestRun(id, Observer(displayName)),
                   disabled = !canRun,
                   tooltip = runContinueTooltip,
                   text = runContinueButton
@@ -182,9 +184,9 @@ object SequenceControl {
     ScalaComponent
       .builder[Props]
       .renderP { ($, p) =>
-        val SequenceControlFocus(_, _, overrides, _, _, control) = p.p
-        val ControlModel(id, partial, nextStep, status, _)       = control
-        val nextStepToRun                                        = nextStep.foldMap(_ + 1)
+        val SequenceControlFocus(_, _, overrides, _, _, control, displayName) = p.p
+        val ControlModel(id, partial, nextStep, status, _)                    = control
+        val nextStepToRun                                                     = nextStep.foldMap(_ + 1)
 
         <.div(
           SeqexecStyles.SequenceControlForm,
@@ -193,7 +195,7 @@ object SequenceControl {
             syncButton(id, p.canSync)
               .when(status.isIdle || status.isError),
             // Run button
-            runButton(id, partial, nextStepToRun, p.canRun)
+            runButton(id, partial, nextStepToRun, p.canRun, displayName)
               .when(status.isIdle || status.isError),
             // Cancel pause button
             cancelPauseButton(id, p.canCancelPause)

gemini-hlsw/observe

modules/web/client/src/main/scala/observe/web/client/circuit/TabFocus.scala
@@ -11,23 +11,23 @@ import observe.model._
 import observe.web.client.model._
 
 final case class TabFocus(
-  canOperate: Boolean,
-  tabs:       NonEmptyList[Either[CalibrationQueueTabActive, AvailableTab]]
-  // defaultObserver: Observer
+  canOperate:  Boolean,
+  tabs:        NonEmptyList[Either[CalibrationQueueTabActive, AvailableTab]],
+  displayName: Option[String]
 )
 
 object TabFocus {
   implicit val eq: Eq[TabFocus] =
-    Eq.by(x => (x.canOperate, x.tabs)) //, x.defaultObserver))
+    Eq.by(x => (x.canOperate, x.tabs, x.displayName))
 
   val tabFocusG: Getter[ObserveAppRootModel, TabFocus] = {
     val getter = ObserveAppRootModel.uiModel.andThen(
       (ObserveUIModel.sequencesOnDisplay
         .andThen(SequencesOnDisplay.availableTabsG))
-      //  .zip(ObserveUIModel.defaultObserverG)
+        .zip(ObserveUIModel.displayNameG)
     )
-    ClientStatus.canOperateG.zip(getter) >>> { case (o, t) =>
-      TabFocus(o, t) //, ob)
+    ClientStatus.canOperateG.zip(getter) >>> { case (o, (t, ob)) =>
+      TabFocus(o, t, ob)
     }
   }
 

modules/web/client/src/main/scala/observe/web/client/circuit/package.scala
@@ -213,7 +213,8 @@ package circuit {
     systemOverrides:        SystemOverrides,
     overrideSubsysControls: SectionVisibilityState,
     canOperate:             Boolean,
-    control:                ControlModel
+    control:                ControlModel,
+    displayName:            String
   )
 
   object SequenceControlFocus {
@@ -224,25 +225,31 @@ package circuit {
          x.systemOverrides,
          x.overrideSubsysControls,
          x.canOperate,
-         x.control
+         x.control,
+         x.displayName
         )
       )
 
     def seqControlG(
       id: Observation.Id
     ): Getter[ObserveAppRootModel, Option[SequenceControlFocus]] = {
-      val getter =
+      val displayNames =
+        ObserveAppRootModel.userLoginFocus.asGetter
+      val tabGetter    =
         ObserveAppRootModel.sequencesOnDisplayL.andThen(SequencesOnDisplay.tabG(id))
-      ClientStatus.canOperateG.zip(getter) >>> {
-        case (status, Some(ObserveTabActive(tab, _))) =>
+      ClientStatus.canOperateG.zip(tabGetter.zip(displayNames)) >>> {
+        case (status,
+              (Some(ObserveTabActive(tab, _)), UserLoginFocus(Some(UserDetails(u, _)), dn))
+            ) =>
           SequenceControlFocus(tab.instrument,
                                tab.obsIdName.id,
                                tab.sequence.systemOverrides,
                                tab.subsystemControlVisible,
                                status,
-                               ControlModel.controlModelG.get(tab)
+                               ControlModel.controlModelG.get(tab),
+                               dn.get(u).orEmpty
           ).some
-        case _                                        => none
+        case _ => none
       }
     }
   }

modules/web/client/src/main/scala/observe/web/client/components/SessionQueueTable.scala
@@ -59,8 +59,10 @@ trait Columns {
   val InstrumentMinWidth    = 90.4333 + ObserveStyles.TableBorderWidth
   val TargetNameColumnWidth = 140.0
   val TargetMinWidth        = 60.0167 + ObserveStyles.TableBorderWidth
-  val ObsNameColumnWidth    = 140.0
+  val ObsNameColumnWidth    = 100.0
   val ObsNameMinWidth       = 89.7340 + ObserveStyles.TableBorderWidth
+  val ObserverColumnWidth   = 100.0
+  val ObserverMinWidth      = 89.7340 + ObserveStyles.TableBorderWidth
 
   sealed trait TableColumn     extends Product with Serializable
   case object IconColumn       extends TableColumn
@@ -71,6 +73,7 @@ trait Columns {
   case object InstrumentColumn extends TableColumn
   case object ObsNameColumn    extends TableColumn
   case object TargetNameColumn extends TableColumn
+  case object ObserverColumn   extends TableColumn
 
   object TableColumn {
     implicit val equal: Eq[TableColumn] = Eq.fromUniversalEquals
@@ -149,14 +152,26 @@ trait Columns {
     width = VariableColumnWidth.unsafeFromDouble(0.25, ObsNameMinWidth)
   )
 
-  val all: NonEmptyList[ColumnMeta[TableColumn]] = NonEmptyList.of(IconColumnMeta,
-                                                                   AddQueueColumnMeta,
-                                                                   ClassColumnMeta,
-                                                                   ObsIdColumnMeta,
-                                                                   StateColumnMeta,
-                                                                   InstrumentColumnMeta,
-                                                                   TargetNameColumnMeta,
-                                                                   ObsNameColumnMeta
+  val ObserverColumnMeta: ColumnMeta[TableColumn] = ColumnMeta[TableColumn](
+    ObserverColumn,
+    name = "observer",
+    label = "Observer",
+    visible = true,
+    removeable = 3,
+    grow = 3,
+    width = VariableColumnWidth.unsafeFromDouble(0.25, ObserverMinWidth)
+  )
+
+  val all: NonEmptyList[ColumnMeta[TableColumn]] = NonEmptyList.of(
+    IconColumnMeta,
+    AddQueueColumnMeta,
+    ClassColumnMeta,
+    ObsIdColumnMeta,
+    StateColumnMeta,
+    InstrumentColumnMeta,
+    TargetNameColumnMeta,
+    ObsNameColumnMeta,
+    ObserverColumnMeta
   )
 
   val allTC = all.map(_.column)
@@ -169,7 +184,8 @@ trait Columns {
     StateColumn      -> StateColumnWidth,
     InstrumentColumn -> InstrumentColumnWidth,
     TargetNameColumn -> TargetNameColumnWidth,
-    ObsNameColumn    -> ObsNameColumnWidth
+    ObsNameColumn    -> ObsNameColumnWidth,
+    ObserverColumn   -> ObserverColumnWidth
   )
 
   val columnsMinWidth: Map[TableColumn, Double] = Map(
@@ -177,7 +193,8 @@ trait Columns {
     StateColumn      -> StateMinWidth,
     InstrumentColumn -> InstrumentMinWidth,
     TargetNameColumn -> TargetMinWidth,
-    ObsNameColumn    -> ObsNameMinWidth
+    ObsNameColumn    -> ObsNameMinWidth,
+    ObserverColumn   -> ObserverMinWidth
   )
 }
 
@@ -198,6 +215,7 @@ object SessionQueueTable extends Columns {
                           s.status,
                           s.instrument,
                           s.targetName,
+                          s.observer,
                           s.name,
                           s.obsClass,
                           s.active,
@@ -292,6 +310,7 @@ object SessionQueueTable extends Columns {
     var status: SequenceState
     var instrument: Instrument
     var targetName: Option[String]
+    var observer: Option[Observer]
     var name: String
     var obsClass: ObsClass
     var active: Boolean
@@ -308,6 +327,7 @@ object SessionQueueTable extends Columns {
       status:        SequenceState,
       instrument:    Instrument,
       targetName:    Option[String],
+      observer:      Option[Observer],
       name:          String,
       obsClass:      ObsClass,
       active:        Boolean,
@@ -321,6 +341,7 @@ object SessionQueueTable extends Columns {
       p.status = status
       p.instrument = instrument
       p.targetName = targetName
+      p.observer = observer
       p.name = name
       p.obsClass = obsClass
       p.active = active
@@ -337,6 +358,7 @@ object SessionQueueTable extends Columns {
         SequenceState,
         Instrument,
         Option[String],
+        Option[Observer],
         String,
         ObsClass,
         Boolean,
@@ -351,6 +373,7 @@ object SessionQueueTable extends Columns {
          l.status,
          l.instrument,
          l.targetName,
+         l.observer,
          l.name,
          l.obsClass,
          l.active,
@@ -557,6 +580,7 @@ object SessionQueueTable extends Columns {
     case InstrumentColumn => linkedTextRenderer(b.props)(_.instrument.show)
     case TargetNameColumn => linkedTextRenderer(b.props)(_.targetName.getOrElse(UnknownTargetName))
     case ObsNameColumn    => linkedTextRenderer(b.props)(_.name)
+    case ObserverColumn   => linkedTextRenderer(b.props)(_.observer.foldMap(_.value))
   }
 
   private val fixedHeaderRenderer: TableColumn => HeaderRenderer[js.Object] = {

modules/web/client/src/main/scala/observe/web/client/components/sequence/toolbars/SequenceControl.scala
@@ -79,8 +79,8 @@ object SequenceControl {
 
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
-  def requestRun(id: Observation.Id): Callback =
-    ObserveCircuit.dispatchCB(RequestRun(id, RunOptions.Normal))
+  def requestRun(id: Observation.Id, observer: Observer): Callback =
+    ObserveCircuit.dispatchCB(RequestRun(id, observer, RunOptions.Normal))
 
   def requestSync(idName: Observation.IdName): Callback =
     ObserveCircuit.dispatchCB(RequestSync(idName))
@@ -104,7 +104,8 @@ object SequenceControl {
     id:                  Observation.Id,
     isPartiallyExecuted: Boolean,
     nextStepToRun:       Int,
-    canRun:              Boolean
+    canRun:              Boolean,
+    displayName:         String
   ) = {
     val runContinueTooltip =
       s"${isPartiallyExecuted.fold("Continue", "Run")} the sequence from the step $nextStepToRun"
@@ -112,7 +113,7 @@ object SequenceControl {
       s"${isPartiallyExecuted.fold("Continue", "Run")} from step $nextStepToRun"
     controlButton(icon = IconPlay,
                   color = Blue,
-                  onClick = requestRun(id),
+                  onClick = requestRun(id, Observer(displayName)),
                   disabled = !canRun,
                   tooltip = runContinueTooltip,
                   text = runContinueButton
@@ -180,9 +181,9 @@ object SequenceControl {
     ScalaComponent
       .builder[Props]
       .renderP { ($, p) =>
-        val SequenceControlFocus(_, _, overrides, _, _, control)  = p.p
-        val ControlModel(idName, partial, nextStepIdx, status, _) = control
-        val nextStepToRunIdx                                      = nextStepIdx.foldMap(_ + 1)
+        val SequenceControlFocus(_, _, overrides, _, _, control, displayName)  = p.p
+        val ControlModel(idName, partial, nextStepIdx, status, _)              = control
+        val nextStepToRunIdx                                                   = nextStepIdx.foldMap(_ + 1)
 
         <.div(
           ObserveStyles.SequenceControlForm,
@@ -191,7 +192,7 @@ object SequenceControl {
             syncButton(idName, p.canSync)
               .when(status.isIdle || status.isError),
             // Run button
-            runButton(idName.id, partial, nextStepToRunIdx, p.canRun)
+            runButton(id, partial, nextStepToRun, p.canRun, displayName)
               .when(status.isIdle || status.isError),
             // Cancel pause button
             cancelPauseButton(idName.id, p.canCancelPause)