gemini-hlsw/seqexec

More events ported
Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

modules/web/client/src/main/scala/seqexec/web/client/components/sequence/steps/StepsControlButtons.scala
@@ -58,14 +58,18 @@ object ControlButtons {
   private def requestGracefulStop(obsId: Observation.Id, name: Observer, stepId: Int): Callback =
     SeqexecCircuit.dispatchCB(RequestGracefulStop(obsId, name, stepId))
 
-  private def requestAbort(obsId: Observation.Id, stepId: Int): Callback =
-    SeqexecCircuit.dispatchCB(RequestAbort(obsId, stepId))
+  private def requestAbort(obsId: Observation.Id, name: Observer, stepId: Int): Callback =
+    SeqexecCircuit.dispatchCB(RequestAbort(obsId, name, stepId))
 
-  private def requestObsPause(obsId: Observation.Id, stepId: Int): Callback =
-    SeqexecCircuit.dispatchCB(RequestObsPause(obsId, stepId))
+  private def requestObsPause(obsId: Observation.Id, name: Observer, stepId: Int): Callback =
+    SeqexecCircuit.dispatchCB(RequestObsPause(obsId, name, stepId))
 
-  private def requestGracefulObsPause(obsId: Observation.Id, stepId: Int): Callback =
-    SeqexecCircuit.dispatchCB(RequestGracefulObsPause(obsId, stepId))
+  private def requestGracefulObsPause(
+    obsId:  Observation.Id,
+    name:   Observer,
+    stepId: Int
+  ): Callback =
+    SeqexecCircuit.dispatchCB(RequestGracefulObsPause(obsId, name, stepId))
 
   private def requestObsResume(obsId: Observation.Id, stepId: Int): Callback =
     SeqexecCircuit.dispatchCB(RequestObsResume(obsId, stepId))
@@ -95,6 +99,7 @@ object ControlButtons {
 
       p.connect { proxy =>
         val isReadingOut = proxy().exists(_.stage === ObserveStage.ReadingOut)
+        val observer     = Observer(p.displayName)
 
         <.div(
           ^.cls := "ui icon buttons",
@@ -106,7 +111,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Teal,
-                  onClick = requestObsPause(p.obsId, p.stepId),
+                  onClick = requestObsPause(p.obsId, observer, p.stepId),
                   disabled = p.requestInFlight || p.isObservePaused || isReadingOut
                 )(IconPause)
               )("Pause the current exposure")
@@ -116,7 +121,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Orange,
-                  onClick = requestStop(p.obsId, Observer(p.displayName), p.stepId),
+                  onClick = requestStop(p.obsId, observer, p.stepId),
                   disabled = p.requestInFlight || isReadingOut
                 )(IconStop)
               )("Stop the current exposure early")
@@ -126,7 +131,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Red,
-                  onClick = requestAbort(p.obsId, p.stepId),
+                  onClick = requestAbort(p.obsId, observer, p.stepId),
                   disabled = p.requestInFlight || isReadingOut
                 )(IconTrash)
               )("Abort the current exposure")
@@ -148,7 +153,7 @@ object ControlButtons {
                   icon = true,
                   color = Teal,
                   basic = true,
-                  onClick = requestObsPause(p.obsId, p.stepId),
+                  onClick = requestObsPause(p.obsId, observer, p.stepId),
                   disabled = p.requestInFlight || p.isObservePaused || isReadingOut
                 )(IconPause)
               )("Pause the current exposure immediately")
@@ -158,7 +163,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Teal,
-                  onClick = requestGracefulObsPause(p.obsId, p.stepId),
+                  onClick = requestGracefulObsPause(p.obsId, observer, p.stepId),
                   disabled =
                     p.requestInFlight || p.isObservePaused || p.nsPendingObserveCmd.isDefined || isReadingOut
                 )(pauseGracefullyIcon)

modules/web/client/src/main/scala/seqexec/web/client/components/sequence/steps/SubsystemControlCell.scala
@@ -30,6 +30,7 @@ import seqexec.web.client.components.SeqexecStyles
 import seqexec.web.client.icons._
 import seqexec.web.client.model.ResourceRunOperation
 import seqexec.web.client.reusability._
+import seqexec.model.Observer
 
 /**
  * Contains the control buttons for each subsystem
@@ -39,7 +40,8 @@ final case class SubsystemControlCell(
   stepId:         Int,
   resources:      List[Resource],
   resourcesCalls: SortedMap[Resource, ResourceRunOperation],
-  canOperate:     Boolean
+  canOperate:     Boolean,
+  displayName:    String
 ) extends ReactProps[SubsystemControlCell](SubsystemControlCell.component)
 
 object SubsystemControlCell {
@@ -48,13 +50,14 @@ object SubsystemControlCell {
   implicit val propsReuse: Reusability[Props] = Reusability.derive[Props]
 
   def requestResourceCall(
-    id:     Observation.Id,
-    stepId: StepId,
-    r:      Resource
+    id:       Observation.Id,
+    stepId:   StepId,
+    observer: Observer,
+    r:        Resource
   ): (ReactMouseEvent, Button.ButtonProps) => Callback =
     (e: ReactMouseEvent, _: Button.ButtonProps) =>
       (e.preventDefaultCB *> e.stopPropagationCB *>
-        SeqexecCircuit.dispatchCB(RequestResourceRun(id, stepId, r)))
+        SeqexecCircuit.dispatchCB(RequestResourceRun(id, observer, stepId, r)))
         .unless_(e.altKey || e.button === StepsTable.MiddleButton)
 
   private val CompletedIcon = IconCheckmark.copy(
@@ -117,7 +120,8 @@ object SubsystemControlCell {
               labelPosition = labeled,
               icon = buttonIcon.isDefined,
               onClickE =
-                if (p.canOperate)(requestResourceCall(p.id, p.stepId, r)) else js.undefined,
+                if (p.canOperate)(requestResourceCall(p.id, p.stepId, Observer(p.displayName), r))
+                else js.undefined,
               clazz = SeqexecStyles.defaultCursor.unless_(p.canOperate)
             )(buttonIcon.whenDefined(identity), r.show)
           )(s"Configure ${r.show}")

modules/web/client/src/main/scala/seqexec/web/client/handlers/OperationsStateHandler.scala
@@ -47,7 +47,7 @@ class OperationsStateHandler[M](modelRW: ModelRW[M, SequencesOnDisplay])
         )
       )
 
-    case RequestAbort(id, _) =>
+    case RequestAbort(id, _, _) =>
       updatedL(
         SequencesOnDisplay.markOperations(
           id,
@@ -104,7 +104,7 @@ class OperationsStateHandler[M](modelRW: ModelRW[M, SequencesOnDisplay])
   }
 
   def handleRequestResourceRun: PartialFunction[Any, ActionResult[M]] = {
-    case RequestResourceRun(id, s, r) =>
+    case RequestResourceRun(id, _, s, r) =>
       updatedL(
         SequencesOnDisplay.markOperations(
           id,

modules/web/client/src/main/scala/seqexec/web/client/handlers/RemoteRequestsHandler.scala
@@ -92,38 +92,40 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
       )
   }
 
-  def handleAbort: PartialFunction[Any, ActionResult[M]] = { case RequestAbort(id, step) =>
-    effectOnly(
-      Effect(
-        SeqexecWebClient
-          .abort(id, step)
-          .as(RunAbort(id))
-          .recover { case _ =>
-            RunAbortFailed(id)
-          }
-      )
-    )
-  }
-
-  def handleObsPause: PartialFunction[Any, ActionResult[M]] = { case RequestObsPause(id, step) =>
-    effectOnly(
-      Effect(
-        SeqexecWebClient
-          .pauseObs(id, step)
-          .as(RunObsPause(id))
-          .recover { case _ =>
-            RunObsPauseFailed(id)
-          }
-      )
-    )
-  }
-
-  def handleGracefulObsPause: PartialFunction[Any, ActionResult[M]] = {
-    case RequestGracefulObsPause(id, step) =>
+  def handleAbort: PartialFunction[Any, ActionResult[M]] = {
+    case RequestAbort(id, observer, step) =>
       effectOnly(
         Effect(
           SeqexecWebClient
-            .pauseObsGracefully(id, step)
+            .abort(id, observer, step)
+            .as(RunAbort(id))
+            .recover { case _ =>
+              RunAbortFailed(id)
+            }
+        )
+      )
+  }
+
+  def handleObsPause: PartialFunction[Any, ActionResult[M]] = {
+    case RequestObsPause(id, observer, step) =>
+      effectOnly(
+        Effect(
+          SeqexecWebClient
+            .pauseObs(id, observer, step)
+            .as(RunObsPause(id))
+            .recover { case _ =>
+              RunObsPauseFailed(id)
+            }
+        )
+      )
+  }
+
+  def handleGracefulObsPause: PartialFunction[Any, ActionResult[M]] = {
+    case RequestGracefulObsPause(id, observer, step) =>
+      effectOnly(
+        Effect(
+          SeqexecWebClient
+            .pauseObsGracefully(id, observer, step)
             .as(RunGracefulObsPause(id))
             .recover { case _ =>
               RunGracefulObsPauseFailed(id)
@@ -150,12 +152,12 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
   }
 
   def handleResourceRun: PartialFunction[Any, ActionResult[M]] = {
-    case RequestResourceRun(id, step, resource) =>
+    case RequestResourceRun(id, observer, step, resource) =>
       val effect = value
         .map(clientId =>
           requestEffect(
             id,
-            SeqexecWebClient.runResource(step, resource, _, clientId),
+            SeqexecWebClient.runResource(step, resource, observer, _, clientId),
             RunResource(_, step, resource),
             RunResourceFailed(_, step, resource, s"Http call to configure ${resource.show} failed")
           )

gemini-hlsw/observe

modules/web/client/src/main/scala/observe/web/client/components/sequence/steps/StepsControlButtons.scala
@@ -57,14 +57,18 @@ object ControlButtons {
   private def requestGracefulStop(obsId: Observation.Id, stepId: StepId): Callback =
     ObserveCircuit.dispatchCB(RequestGracefulStop(obsId, stepId))
 
-  private def requestAbort(obsIdName: Observation.IdName, stepId: StepId): Callback =
-    ObserveCircuit.dispatchCB(RequestAbort(obsIdName, stepId))
+  private def requestAbort(obsIdName: Observation.IdName, name: Observer, stepId: StepId): Callback =
+    ObserveCircuit.dispatchCB(RequestAbort(obsIdName, name, stepId))
 
-  private def requestObsPause(obsId: Observation.Id, stepId: StepId): Callback =
-    ObserveCircuit.dispatchCB(RequestObsPause(obsId, stepId))
+  private def requestObsPause(obsId: Observation.Id, name: Observer, stepId: StepId): Callback =
+    ObserveCircuit.dispatchCB(RequestObsPause(obsId, name, stepId))
 
-  private def requestGracefulObsPause(obsId: Observation.Id, stepId: StepId): Callback =
-    ObserveCircuit.dispatchCB(RequestGracefulObsPause(obsId, stepId))
+  private def requestGracefulObsPause(
+    obsId:  Observation.Id,
+    name:   Observer,
+    stepId: Int
+  ): Callback =
+    SeqexecCircuit.dispatchCB(RequestGracefulObsPause(obsId, name, stepId))
 
   private def requestObsResume(obsId: Observation.Id, stepId: StepId): Callback =
     ObserveCircuit.dispatchCB(RequestObsResume(obsId, stepId))
@@ -94,6 +98,7 @@ object ControlButtons {
 
       p.connect { proxy =>
         val isReadingOut = proxy().exists(_.stage === ObserveStage.ReadingOut)
+        val observer     = Observer(p.displayName)
 
         <.div(
           ^.cls := "ui icon buttons",
@@ -105,7 +110,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Teal,
-                  onClick = requestObsPause(p.obsIdName.id, p.stepId),
+                  onClick = requestObsPause(p.obsIdName, observer, p.stepId),
                   disabled = p.requestInFlight || p.isObservePaused || isReadingOut
                 )(IconPause)
               )("Pause the current exposure")
@@ -115,7 +120,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Orange,
-                  onClick = requestStop(p.obsIdName, Observer(p.displayName), p.stepId),
+                  onClick = requestStop(p.obsIdName, observer, p.stepId),
                   disabled = p.requestInFlight || isReadingOut
                 )(IconStop)
               )("Stop the current exposure early")
@@ -125,7 +130,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Red,
-                  onClick = requestAbort(p.obsIdName, p.stepId),
+                  onClick = requestAbort(p.obsIdName, observer, p.stepId),
                   disabled = p.requestInFlight || isReadingOut
                 )(IconTrash)
               )("Abort the current exposure")
@@ -147,7 +152,7 @@ object ControlButtons {
                   icon = true,
                   color = Teal,
                   basic = true,
-                  onClick = requestObsPause(p.obsIdName.id, p.stepId),
+                  onClick = requestObsPause(p.obsIdName, observer, p.stepId),
                   disabled = p.requestInFlight || p.isObservePaused || isReadingOut
                 )(IconPause)
               )("Pause the current exposure immediately")
@@ -157,7 +162,7 @@ object ControlButtons {
                 trigger = Button(
                   icon = true,
                   color = Teal,
-                  onClick = requestGracefulObsPause(p.obsIdName.id, p.stepId),
+                  onClick = requestGracefulObsPause(p.obsIdName, observer, p.stepId),
                   disabled =
                     p.requestInFlight || p.isObservePaused || p.nsPendingObserveCmd.isDefined || isReadingOut
                 )(pauseGracefullyIcon)

modules/web/client/src/main/scala/observe/web/client/components/sequence/steps/SubsystemControlCell.scala
@@ -28,6 +28,7 @@ import observe.web.client.components.ObserveStyles
 import observe.web.client.icons._
 import observe.web.client.model.ResourceRunOperation
 import observe.web.client.reusability._
+import observe.model.Observer
 
 /**
  * Contains the control buttons for each subsystem
@@ -37,7 +38,8 @@ final case class SubsystemControlCell(
   stepId:         StepId,
   resources:      List[Resource],
   resourcesCalls: SortedMap[Resource, ResourceRunOperation],
-  canOperate:     Boolean
+  canOperate:     Boolean,
+  displayName:    String
 ) extends ReactProps[SubsystemControlCell](SubsystemControlCell.component)
 
 object SubsystemControlCell {
@@ -48,11 +50,12 @@ object SubsystemControlCell {
   def requestResourceCall(
     idName: Observation.IdName,
     stepId: StepId,
+    observer: Observer,
     r:      Resource
   ): (ReactMouseEvent, Button.ButtonProps) => Callback =
     (e: ReactMouseEvent, _: Button.ButtonProps) =>
       (e.preventDefaultCB *> e.stopPropagationCB *>
-        ObserveCircuit.dispatchCB(RequestResourceRun(idName, stepId, r)))
+        ObserveCircuit.dispatchCB(RequestResourceRun(idName, observer, stepId, r)))
         .unless_(e.altKey || e.button === StepsTable.MiddleButton)
 
   private val CompletedIcon = IconCheckmark.copy(
@@ -115,7 +118,7 @@ object SubsystemControlCell {
               labelPosition = labeled,
               icon = buttonIcon.isDefined,
               onClickE =
-                if (p.canOperate) requestResourceCall(p.idName, p.stepId, r) else js.undefined,
+                if (p.canOperate) requestResourceCall(p.idName, p.stepId, Observer(p.displayName), r) else js.undefined,
               clazz = ObserveStyles.defaultCursor.unless_(p.canOperate)
             )(buttonIcon.whenDefined(identity), r.show)
           )(s"Configure ${r.show}")

modules/web/client/src/main/scala/observe/web/client/handlers/OperationsStateHandler.scala
@@ -48,7 +48,7 @@ class OperationsStateHandler[M](modelRW: ModelRW[M, SequencesOnDisplay])
         )
       )
 
-    case RequestAbort(idName, _) =>
+    case RequestAbort(idName, _, _) =>
       updatedL(
         SequencesOnDisplay.markOperations(
           idName.id,
@@ -105,7 +105,7 @@ class OperationsStateHandler[M](modelRW: ModelRW[M, SequencesOnDisplay])
   }
 
   def handleRequestResourceRun: PartialFunction[Any, ActionResult[M]] = {
-    case RequestResourceRun(idName, s, r) =>
+    case RequestResourceRun(idName, _, s, r) =>
       updatedL(
         SequencesOnDisplay.markOperations(
           idName.id,

modules/web/client/src/main/scala/observe/web/client/handlers/RemoteRequestsHandler.scala
@@ -92,38 +92,40 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
       )
   }
 
-  def handleAbort: PartialFunction[Any, ActionResult[M]] = { case RequestAbort(idName, step) =>
-    effectOnly(
-      Effect(
-        ObserveWebClient
-          .abort(idName.id, step)
-          .as(RunAbort(idName.id))
-          .recover { case _ =>
-            RunAbortFailed(idName)
-          }
-      )
-    )
-  }
-
-  def handleObsPause: PartialFunction[Any, ActionResult[M]] = { case RequestObsPause(id, step) =>
-    effectOnly(
-      Effect(
-        ObserveWebClient
-          .pauseObs(id, step)
-          .as(RunObsPause(id))
-          .recover { case _ =>
-            RunObsPauseFailed(id)
-          }
-      )
-    )
-  }
-
-  def handleGracefulObsPause: PartialFunction[Any, ActionResult[M]] = {
-    case RequestGracefulObsPause(id, step) =>
+  def handleAbort: PartialFunction[Any, ActionResult[M]] = {
+    case RequestAbort(idName, observer, step) =>
       effectOnly(
         Effect(
           ObserveWebClient
-            .pauseObsGracefully(id, step)
+            .abort(idName.id, observer, step)
+            .as(RunAbort(id))
+            .recover { case _ =>
+              RunAbortFailed(id)
+            }
+        )
+      )
+  }
+
+  def handleObsPause: PartialFunction[Any, ActionResult[M]] = {
+    case RequestObsPause(idName, observer, step) =>
+      effectOnly(
+        Effect(
+          ObserveWebClient
+            .pauseObs(id, observer, step)
+            .as(RunObsPause(id))
+            .recover { case _ =>
+              RunObsPauseFailed(id)
+            }
+        )
+      )
+  }
+
+  def handleGracefulObsPause: PartialFunction[Any, ActionResult[M]] = {
+    case RequestGracefulObsPause(id, observer, step) =>
+      effectOnly(
+        Effect(
+          ObserveWebClient
+            .pauseObsGracefully(id, observer, step)
             .as(RunGracefulObsPause(id))
             .recover { case _ =>
               RunGracefulObsPauseFailed(id)
@@ -150,12 +152,12 @@ class RemoteRequestsHandler[M](modelRW: ModelRW[M, Option[ClientId]])
   }
 
   def handleResourceRun: PartialFunction[Any, ActionResult[M]] = {
-    case RequestResourceRun(idName, step, resource) =>
+    case RequestResourceRun(idName, observer, step, resource) =>
       val effect = value
         .map(clientId =>
           requestEffect(
             idName,
-            ObserveWebClient.runResource(step, resource, _, clientId),
+            ObserveWebClient.runResource(step, resource, observer, _, clientId),
             RunResource(_, step, resource),
             RunResourceFailed(_, step, resource, s"Http call to configure ${resource.show} failed")
           )