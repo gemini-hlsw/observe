90987b875714ce6c08886cbdd7adbdf4cacb478e
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2023-10-11 14:37:45 -0300

    Restore the load route

--- modules/web/server/src/main/scala/observe/web/server/http4s/vars.scala
+++ modules/web/server/src/main/scala/observe/web/server/http4s/vars.scala
@@ -4,9 +4,30 @@
 package observe.web.server.http4s
 
 import cats.syntax.all.*
+import lucuma.core.model.Observation
+import lucuma.core.util.Enumerated
 import observe.model.ClientId
+import observe.model.Observer
+import observe.model.enums.Instrument
 
 object ClientIDVar {
   def unapply(str: String): Option[ClientId] =
     Either.catchNonFatal(ClientId(java.util.UUID.fromString(str))).toOption
+}
+
+trait EnumeratedVar[A: Enumerated] {
+  def unapply(str: String): Option[A] =
+    Enumerated[A].all.find(a => Enumerated[A].fromTag(str).exists(_ === a))
+}
+
+object InstrumentVar extends EnumeratedVar[Instrument]
+
+object ObsIdVar {
+  def unapply(str: String): Option[Observation.Id] =
+    Observation.Id.parse(str)
+}
+
+object ObserverVar {
+  def unapply(str: String): Option[Observer] =
+    Observer(str).some
 }
