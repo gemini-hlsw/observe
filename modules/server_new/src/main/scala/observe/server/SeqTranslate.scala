6ef53286499260486d9865546ed98064cb778ae4
Raúl Piaggio <rpiaggio@gemini.edu>
2024-03-05 18:28:16 -0300

    refactor "unreachable case"s

--- modules/server_new/src/main/scala/observe/server/SeqTranslate.scala
+++ modules/server_new/src/main/scala/observe/server/SeqTranslate.scala
@@ -323,8 +323,9 @@ object SeqTranslate {
           .exists(isObserving)
         stId   <- obsSeq.seq.currentStep.map(_.id)
         curStp <- obsSeq.seqGen.steps.find(_.id === stId)
-        obsCtr <- curStp.some.collect { case x: SequenceGen.PendingStepGen[F] =>
-                    x.obsControl
+        obsCtr <- curStp.some.collect {
+                    case SequenceGen.PendingStepGen(_, _, _, obsControl, _, _, _, _, _) =>
+                      obsControl
                   }
       } yield Stream.eval(
         f(obsCtr(obsSeq.overrides)).attempt
