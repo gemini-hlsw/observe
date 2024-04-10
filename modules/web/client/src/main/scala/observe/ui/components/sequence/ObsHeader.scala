09506134c8b25ac4df38fbcde0330f3cb978955b
Hugo van Rijswijk <git@hugovr.nl>
2024-04-10 18:54:16 +0200

    Add button to expand/collapse all visits

--- modules/web/client/src/main/scala/observe/ui/components/sequence/ObsHeader.scala
+++ modules/web/client/src/main/scala/observe/ui/components/sequence/ObsHeader.scala
@@ -42,6 +42,7 @@ object ObsHeader:
           s"${props.observation.title} [${props.observation.obsId}]"
         ),
         <.div(ObserveStyles.ObsSummaryDetails)(
+          <.span(^.id := "sequence-table-expand-all"),
           <.span(props.observation.configurationSummary),
           <.span(props.observation.constraintsSummary),
           props.overrides
