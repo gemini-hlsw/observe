7b28c247a3b3594bd9f8e1cc249977fb6324a5ab
Raúl Piaggio <rpiaggio@gemini.edu>
2025-05-01 12:50:49 -0300

    fix scripts, readme

--- deploy/scripts/install.sh
+++ deploy/scripts/install.sh
@@ -2,8 +2,9 @@
 
 mkdir ~/observe
 cd ~/observe
-https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/config.sh >config.sh
-https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/start.sh >start.sh
-https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/stop.sh >stop.sh
-https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/update.sh >update.sh
+curl https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/config.sh >config.sh
+curl https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/start.sh >start.sh
+curl https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/stop.sh >stop.sh
+curl https://raw.githubusercontent.com/gemini-hlsw/observe/refs/heads/main/deploy/scripts/update.sh >update.sh
 chmod +x *.sh
+mkdir ~/observe/conf
